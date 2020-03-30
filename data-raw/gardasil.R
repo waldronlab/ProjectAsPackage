f3d7a0a606243695f78167e887f351804adad61c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-03-30 11:07:53 +0000

    Deploy waldronlab/ProjectAsPackage to github.com/waldronlab/ProjectAsPackage.git:gh-pages

--- data-raw/gardasil.R
+++
@@ -1,4 +0,0 @@
-## code to prepare `gardasil` dataset goes here
-## The base for this file was created using `usethis::use_data_raw("gardasil")`
-gardasil <- ProjectAsPackage::read_gardasil(other.as.NA = TRUE)
-usethis::use_data(gardasil, overwrite = TRUE)
