e269119389cb4b09103ef669a8351f84fdac4077
Levi Waldron <lwaldron.research@gmail.com>
2020-03-30 18:05:50 +0200

    try adding and staging docs

--- update.sh
+++ update.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 # Use pkgdown to build a basic package website
+
+## To create an initially empty gh-pages branch, where GitHub Pages will be deployed:
+# git checkout --orphan gh-pages
+# git rm -rf .
+# git commit --allow-empty -m 'Initial gh-pages commit'
+# git push origin gh-pages
+# git checkout master
+
+## For subsequent manual updates, if you are not using the procedure in .travis.yml
 Rscript --vanilla -e "pkgdown::build_site(lazy=TRUE)"
-git add docs/
-git stage docs/
+git checkout --orphan gh-pages
+git pull origin gh-pages
+mv docs/* .
+rm -rf docs/
+git stage *
 git commit -m "update GitHub pages"
-git push
+git push origin gh-pages
